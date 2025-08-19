c166a4da9cb61715e192f6826b7ef83e7edad75b
chuba <chinemelumchubanwene57@gmail.com>
2025-08-19 18:33:52 +0100

    fix build errors

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -97,7 +97,7 @@ function FormLabel({
             data-slot="form-label"
             data-error={!!error}
             className={cn("data-[error=true]:text-destructive", className)}
-            for={formItemId}
+            htmlFor={formItemId}
             {...props}
         />
     );
